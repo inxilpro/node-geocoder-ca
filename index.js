5f31a293eaa9cb2524257230b912a7f93833f111
Chris Morrell <github@rchy.net>
2013-08-06 21:23:29 -0400

    Initial implementation

---
+++ index.js
@@ -0,0 +1,163 @@
+var http = require('http'),
+	qs = require('querystring'),
+	EventEmitter = require('events').EventEmitter,
+	util = require('util'),
+	sax = require('sax');
+
+/**
+ * Basic coordinates object
+ *
+ * @param {Number} lat
+ * @param {Number} lon
+ * @constructor
+ */
+function Coords(lat, lon) {
+	this.lat = lat;
+	this.lon = lon;
+}
+
+/**
+ * Geocoder object
+ *
+ * @param {Object} options
+ * @constructor
+ */
+function Geocoder(options) {
+	this.options = options || {};
+	EventEmitter.call(this);
+}
+
+// Add event handling to geocoder
+util.inherits(Geocoder, EventEmitter);
+
+Geocoder.endpoints = {
+	'PRIMARY': 'http://geocoder.ca',
+	'BACKUP': 'http://backup-geocoder.ca'
+};
+
+/**
+ * Do geocode
+ *
+ * @param {Object} location - Fields to query based on
+ * @param {Function} callback - Receives err, coords
+ */
+Geocoder.prototype.geocode = function(location, callback) {
+	var self = this,
+		request, qs;
+
+	// Make a guess at location
+	if (!location || '' == location) {
+		var err = new Error('Invalid location');
+		return this._error(err, callback);
+	} else if ('string' == typeof location) {
+		request = {
+			locate: location
+		};
+	} else if ('object' == typeof location) {
+		request = location;
+	} else if ('number' == typeof location && 5 == location.toString().length) {
+		request = {
+			postal: location
+		};
+	} else {
+		var err = new Error('Invalid location');
+		return this._error(err, callback);
+	}
+
+	// Set up Coords
+	this.coords = new Coords();
+
+	// Build qs
+	qs = this._getQueryString(request);
+
+	// Try main endpoint
+	http.get(Geocoder.endpoints.PRIMARY + '?' + qs, function(result) {
+		// Process results on success
+		self._processHttpResult.call(self, result, callback);
+	}).on('error', function(err) {
+		// If we have an error and we're a paying user, try again at the backup endpoint
+		if (self.options.apiKey) {
+			http.get(Geocoder.endpoints.BACKUP + '?' + qs, function(result) {
+				// Process results on backup success
+				self._processHttpResult.call(self, result, callback);
+			}).on('error', function(err) {
+				// OK, now we really have an error
+				self._error(err, callback);
+			});
+		} else {
+			// Throw error if we're a free account
+			self._error(err, callback);
+		}
+	});
+}
+
+Geocoder.prototype._processHttpResult = function(result, callback) {
+	var self = this,
+		currentNode = '',
+		saxOpts = {
+			trim: true,
+			normalize: true,
+			lowercase: true
+		},
+		stream = sax.createStream(false, saxOpts)
+			.on('opentag', function(node) {
+				currentNode = node.name;
+			})
+			.on('text', function(text) {
+				switch (currentNode) {
+					case 'latt':
+						self.coords.lat = parseFloat(text);
+						break;
+					case 'longt':
+						self.coords.lon = parseFloat(text);
+						break;
+				}
+			})
+			.on('end', function() {
+				if (self.coords.lat && self.coords.lon) {
+					this.emit("result", self.coords);
+					callback.call(self.coords, null, self.coords);
+				} else {
+					var err = new Error('Unable to geocode location.');
+					self._error(err, callback);
+				}
+			})
+			.on('error', function(err) {
+				self._error(err, callback);
+			});
+
+	// Pipe response to SAX
+	result.pipe(stream);
+}
+
+Geocoder.prototype._error = function(err, callback) {
+	// this.emit('error', err);
+	callback(err, null);
+	return err;
+}
+
+/**
+ * Helper method to build an appropriate query string for the Geocoder.ca API
+ *
+ * @param {Object} request
+ * @returns {String}
+ * @throws Error
+ * @private
+ */
+Geocoder.prototype._getQueryString = function(request) {
+	// Set up request
+	request.geoit = 'XML';
+	if (this.options.apiKey) {
+		request.auth = this.options.apiKey;
+	}
+
+	if ((!request.locate) && (!request.postal) && !(request.addresst && request.stno && request.city && request.prov)) {
+		throw new Error('No required location information was provided (locate or postal or addresst/stno/city/prov');
+	}
+
+	return qs.stringify(request);
+};
+
+// Export
+exports.Geocoder = Geocoder;
+exports.Coords = Coords;
--- test/index.js
+++ test/index.js
@@ -1,41 +1,162 @@
 var vows = require('vows'),
 	assert = require('assert'),
-	geocoder = require('../index.js');
+	geo = require('../index.js'),
+	Coords = geo.Coords,
+	geocoder = new geo.Geocoder(),
+	simple_distance = require('./simple_distance.js')
+	specReporter = require("vows/lib/vows/reporters/spec");
 
 // Geocoder Test Suite
-vows.describe('Geocoding').addBatch({
-	'A valid, full address': {
-		topic: geocoder.geocode('525 Market St, Philadelphia, PA 19106'),
-
-		'does not throw an exception': function (response) {
-			assert.doesNotThrow(response, Error);
+vows.describe('When geocoding').addBatch({
+	'A valid, full US address': {
+		topic: function() {
+			geocoder.geocode('525 Market St, Philadelphia, PA 19106', this.callback);
 		},
-		'returns coords': function (response) {
-			assert.instanceOf(response, geocoder.Coords);
+
+		'does not return an error': function (err, coords) {
+			assert.isNull(err);
+		},
+		'returns coords': function (err, coords) {
+			assert.instanceOf(coords, Coords);
+		},
+		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
+			var precalcd = { lat: 39.950598, lon: -75.149274 },
+				distance = simple_distance(coords, precalcd);
+
+			assert(distance < 0.5);
 		}
 	},
 	'A valid, partial address': {
-		topic: geocoder.geocode('525 Market 19106')
+		topic: function() {
+			geocoder.geocode('525 Market 19106', this.callback);
+		},
+
+		'does not return an error': function (err, coords) {
+			assert.isNull(err);
+		},
+		'returns coords': function (err, coords) {
+			assert.instanceOf(coords, Coords);
+		},
+		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
+			var precalcd = { lat: 39.950598, lon: -75.149274 },
+				distance = simple_distance(coords, precalcd);
+
+			assert(distance < 0.5);
+		}
+	},
+	'An ambiguous address': {
+		topic: function() {
+			geocoder.geocode('Market St Philadelphia PA 19106', this.callback);
+		},
+
+		'returns an error': function (err, coords) {
+			assert.isNotNull(err);
+			assert.instanceOf(err, Error);
+		}
 	},
 	'A US zip code (as a string)': {
-		topic: geocoder.geocode('19106')
+		topic: function() {
+			geocoder.geocode('19106', this.callback);
+		},
+
+
+		'does not return an error': function (err, coords) {
+			assert.isNull(err);
+		},
+		'returns coords': function (err, coords) {
+			assert.instanceOf(coords, Coords);
+		},
+		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
+			var precalcd = { lat: 39.947420, lon: -75.147271 },
+				distance = simple_distance(coords, precalcd);
+
+			assert(distance < 0.5);
+		}
 	},
 	'A US zip code (as a Number)': {
-		topic: geocoder.geocode(19106)
+		topic: function() {
+			geocoder.geocode(19106, this.callback);
+		},
+
+		'does not return an error': function (err, coords) {
+			assert.isNull(err);
+		},
+		'returns coords': function (err, coords) {
+			assert.instanceOf(coords, Coords);
+		},
+		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
+			var precalcd = { lat: 39.947420, lon: -75.147271 },
+				distance = simple_distance(coords, precalcd);
+
+			assert(distance < 0.5);
+		}
 	},
 	'A Canadian postal code (with space)': {
-		topic: geocoder.geocode('M4A 2L7')
+		topic: function() {
+			geocoder.geocode('M4A 2L7', this.callback);
+		},
+
+		'does not return an error': function (err, coords) {
+			assert.isNull(err);
+		},
+		'returns coords': function (err, coords) {
+			assert.instanceOf(coords, Coords);
+		},
+		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
+			var precalcd = { lat: 43.718971, lon: -79.300228 },
+				distance = simple_distance(coords, precalcd);
+
+			assert(distance < 0.5);
+		}
 	},
 	'A Canadian postal code (without space)': {
-		topic: geocoder.geocode('M4A2L7')
+		topic: function() {
+			geocoder.geocode('M4A2L7', this.callback);
+		},
+
+		'does not return an error': function (err, coords) {
+			assert.isNull(err);
+		},
+		'returns coords': function (err, coords) {
+			assert.instanceOf(coords, Coords);
+		},
+		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
+			var precalcd = { lat: 43.718971, lon: -79.300228 },
+				distance = simple_distance(coords, precalcd);
+
+			assert(distance < 0.5);
+		}
 	},
 	'An invalid address': {
-		topic: geocoder.geocode('999999 Market St Philadelphia PA 19147')
+		topic: function() {
+			geocoder.geocode('999999 Market St Philadelphia PA 19147', this.callback);
+		},
+
+		'returns an error': function (err, coords) {
+			assert.isNotNull(err);
+			assert.instanceOf(err, Error);
+		}
 	},
 	'An empty string': {
-		topic: geocoder.geocode('')
+		topic: function() {
+			geocoder.geocode('', this.callback);
+		},
+
+		'returns an error': function (err, coords) {
+			assert.isNotNull(err);
+			assert.instanceOf(err, Error);
+		}
 	},
-	'A non-string value': {
-		topic: geocoder.geocode(false)
+	'A non-String/non-Number value': {
+		topic: function() {
+			geocoder.geocode(false, this.callback);
+		},
+
+		'returns an error': function (err, coords) {
+			assert.isNotNull(err);
+			assert.instanceOf(err, Error);
+		}
 	}
-}).run();
+}).run({
+	reporter: specReporter
+});
