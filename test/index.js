d4c244d3ad624636aac48f151317d0671869e855
Chris Morrell <github@rchy.net>
2013-08-07 11:30:10 -0400

    Moved classes to lib/; added distanceTo() calc to Coords; updated tests

--- test/index.js
+++ test/index.js
@@ -3,15 +3,38 @@ var vows = require('vows'),
 	geo = require('../index.js'),
 	Coords = geo.Coords,
 	geocoder = new geo.Geocoder(),
-	simple_distance = require('./simple_distance.js'),
 	specReporter = require("vows/lib/vows/reporters/spec"),
-	validAddress = '525 Market St, Philadelphia, PA 19106';
+	testData = {
+		validAddress: {
+			address: '525 Market St, Philadelphia, PA 19106',
+			point: [39.950598, -75.149274]
+		},
+		partialAddress: {
+			address: '525 Market 19106',
+			point: [39.950598, -75.149274]
+		},
+		ambiguousAddress: {
+			address: 'Market St Philadelphia PA 19106'
+		},
+		usZip: {
+			address: 19106,
+			point: [39.947420, -75.147271]
+		},
+		caProvinceWithSpace: {
+			address: 'M4A 2L7',
+			point: [43.718971, -79.300228]
+		},
+		caProvinceNoSpace: {
+			address: 'M4A2L7',
+			point: [43.718971, -79.300228]
+		}
+	};
 
 // Geocoder Test Suite
 vows.describe('When geocoding').addBatch({
 	'A valid, full US address': {
 		topic: function() {
-			geocoder.geocode(validAddress, this.callback);
+			geocoder.geocode(testData.validAddress.address, this.callback);
 		},
 
 		'does not return an error': function (err, coords) {
@@ -20,16 +43,14 @@ vows.describe('When geocoding').addBatch({
 		'returns coords': function (err, coords) {
 			assert.instanceOf(coords, Coords);
 		},
-		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
-			var precalcd = { lat: 39.950598, lon: -75.149274 },
-				distance = simple_distance(coords, precalcd);
-
-			assert(distance < 0.5);
+		'coords are within 0.25 miles of pre-calculated point': function(err, coords) {
+			var distance = coords.distanceTo(testData.validAddress.point);
+			assert(distance < 0.25);
 		}
 	},
 	'A valid, partial address': {
 		topic: function() {
-			geocoder.geocode('525 Market 19106', this.callback);
+			geocoder.geocode(testData.partialAddress.address, this.callback);
 		},
 
 		'does not return an error': function (err, coords) {
@@ -38,16 +59,14 @@ vows.describe('When geocoding').addBatch({
 		'returns coords': function (err, coords) {
 			assert.instanceOf(coords, Coords);
 		},
-		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
-			var precalcd = { lat: 39.950598, lon: -75.149274 },
-				distance = simple_distance(coords, precalcd);
-
-			assert(distance < 0.5);
+		'coords are within 0.25 miles of pre-calculated point': function(err, coords) {
+			var distance = coords.distanceTo(testData.partialAddress.point);
+			assert(distance < 0.25);
 		}
 	},
 	'An ambiguous address': {
 		topic: function() {
-			geocoder.geocode('Market St Philadelphia PA 19106', this.callback);
+			geocoder.geocode(testData.ambiguousAddress.address, this.callback);
 		},
 
 		'returns an error': function (err, coords) {
@@ -57,7 +76,7 @@ vows.describe('When geocoding').addBatch({
 	},
 	'A US zip code (as a string)': {
 		topic: function() {
-			geocoder.geocode('19106', this.callback);
+			geocoder.geocode(testData.usZip.address.toString(), this.callback);
 		},
 
 
@@ -67,16 +86,14 @@ vows.describe('When geocoding').addBatch({
 		'returns coords': function (err, coords) {
 			assert.instanceOf(coords, Coords);
 		},
-		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
-			var precalcd = { lat: 39.947420, lon: -75.147271 },
-				distance = simple_distance(coords, precalcd);
-
-			assert(distance < 0.5);
+		'coords are within 0.25 miles of pre-calculated point': function(err, coords) {
+			var distance = coords.distanceTo(testData.usZip.point);
+			assert(distance < 0.25);
 		}
 	},
 	'A US zip code (as a Number)': {
 		topic: function() {
-			geocoder.geocode(19106, this.callback);
+			geocoder.geocode(testData.usZip.address, this.callback);
 		},
 
 		'does not return an error': function (err, coords) {
@@ -85,16 +102,14 @@ vows.describe('When geocoding').addBatch({
 		'returns coords': function (err, coords) {
 			assert.instanceOf(coords, Coords);
 		},
-		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
-			var precalcd = { lat: 39.947420, lon: -75.147271 },
-				distance = simple_distance(coords, precalcd);
-
-			assert(distance < 0.5);
+		'coords are within 0.25 miles of pre-calculated point': function(err, coords) {
+			var distance = coords.distanceTo(testData.usZip.point);
+			assert(distance < 0.25);
 		}
 	},
 	'A Canadian postal code (with space)': {
 		topic: function() {
-			geocoder.geocode('M4A 2L7', this.callback);
+			geocoder.geocode(testData.caProvinceWithSpace.address, this.callback);
 		},
 
 		'does not return an error': function (err, coords) {
@@ -103,16 +118,14 @@ vows.describe('When geocoding').addBatch({
 		'returns coords': function (err, coords) {
 			assert.instanceOf(coords, Coords);
 		},
-		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
-			var precalcd = { lat: 43.718971, lon: -79.300228 },
-				distance = simple_distance(coords, precalcd);
-
-			assert(distance < 0.5);
+		'coords are within 0.25 miles of pre-calculated point': function(err, coords) {
+			var distance = coords.distanceTo(testData.caProvinceWithSpace.point);
+			assert(distance < 0.25);
 		}
 	},
 	'A Canadian postal code (without space)': {
 		topic: function() {
-			geocoder.geocode('M4A2L7', this.callback);
+			geocoder.geocode(testData.caProvinceNoSpace.address, this.callback);
 		},
 
 		'does not return an error': function (err, coords) {
@@ -121,11 +134,9 @@ vows.describe('When geocoding').addBatch({
 		'returns coords': function (err, coords) {
 			assert.instanceOf(coords, Coords);
 		},
-		'coords are within 1/2 km of pre-calculated coords': function(err, coords) {
-			var precalcd = { lat: 43.718971, lon: -79.300228 },
-				distance = simple_distance(coords, precalcd);
-
-			assert(distance < 0.5);
+		'coords are within 0.25 miles of pre-calculated point': function(err, coords) {
+			var distance = coords.distanceTo(testData.caProvinceNoSpace.point);
+			assert(distance < 0.25);
 		}
 	},
 	'An invalid address': {
